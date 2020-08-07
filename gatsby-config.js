3f1292a364615432e73319c2eba7da63af7ecf92
Luiz Pires <lutidev@gmail.com>
2020-08-07 10:33:07 -0300

    add avatar image

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,35 +1,35 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby PWA studies`,
+    title: 'Gatsby PWA studies',
     position: 'JavaScript Developer',
-    description: `Este site foi criado para armazenar meus estudos sobre Gatsby, PWA, GraphQL, Netlify CMS...`,
-    author: `@lutidev`,
+    description: 'Este site foi criado para armazenar meus estudos sobre Gatsby, PWA, GraphQL, Netlify CMS...',
+    author: '@lutidev'
   },
   plugins: [
-    `gatsby-plugin-react-helmet`,
+    'gatsby-plugin-react-helmet',
     {
-      resolve: `gatsby-source-filesystem`,
+      resolve: 'gatsby-source-filesystem',
       options: {
-        name: `images`,
-        path: `${__dirname}/src/images`,
-      },
+        name: 'images',
+        path: `${__dirname}/src/images`
+      }
     },
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
+    'gatsby-transformer-sharp',
+    'gatsby-plugin-sharp',
     {
-      resolve: `gatsby-plugin-manifest`,
+      resolve: 'gatsby-plugin-manifest',
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
-        start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
-      },
-    },
+        name: 'gatsby-starter-default',
+        short_name: 'starter',
+        start_url: '/',
+        background_color: '#663399',
+        theme_color: '#663399',
+        display: 'minimal-ui',
+        icon: 'src/images/gatsby-icon.png' // This path is relative to the root of the site.
+      }
+    }
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
-  ],
+  ]
 }
