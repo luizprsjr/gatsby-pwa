1134e527502cced88a1c9ce06816a9495bd7c456
Luiz Pires <lutidev@gmail.com>
2020-08-07 12:39:55 -0300

    add layout component

---
+++ src/components/Layout/index.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import Profile from '../Profile'
+
+import styled from 'styled-components'
+
+const LayoutWrapper = styled.section`
+  display: flex;
+`
+const LayoutMain = styled.main`
+  background: #20212A;
+  min-height: 100vh;
+  padding: 0 3.75rem 0 20rem;
+  width: 100%;
+`
+
+const Layout = ({ children }) => {
+  return (
+    <LayoutWrapper>
+      <LayoutMain>{children}</LayoutMain>
+      <aside>
+        <Profile />
+      </aside>
+    </LayoutWrapper>
+  )
+}
+
+Layout.propTypes = {
+  children: PropTypes.node.isRequired
+}
+
+export default Layout
